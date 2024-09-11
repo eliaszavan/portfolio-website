5e2bc7e639812bee3a1334903cec7dab27c32a7a
eliaszavan <eliaszavan@gmail.com>
2024-09-11 15:54:29 -0300

    Added Mobile Menu + Animations

--- js/mobile-navbar.js
+++ js/mobile-navbar.js
@@ -4,11 +4,26 @@ class MobileNavbar {
       this.navList = document.querySelector(navList);
       this.navLinks = document.querySelectorAll(navLinks);
       this.activeClass = "active";
+
+      this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks () {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation ? (link.style.animation = "") :
+        (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`)
+      });
+    }
+
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.animateLinks();
     }
 
   
     addClickEvent() {
-      this.mobileMenu.addEventListener("click", () => console.log("HEYYY"));
+      this.mobileMenu.addEventListener("click", this.handleClick);
     }
   
     init() {
